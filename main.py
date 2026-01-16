ffe9ec439fdc987814dba8bdb1eef30fcf2adf3a
PritBPatel1845 <pritpatel1845@gmail.com>
2026-01-16 12:18:35 -0400

    split the train and test

--- main.py
+++ main.py
@@ -50,3 +50,36 @@ plt.show()
 plt.figure(figsize=(20, 15))
 data.hist(bins=50, figsize=(20,15))
 plt.show()
+
+data["income_cat"]= pd.cut(data["median_income"],
+                              bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                              labels=[1, 2, 3, 4, 5])
+
+data['income_cat'].value_counts().plot(kind='bar', figsize=(10,6))
+plt.xlabel('Income Category')
+plt.ylabel('Number of Households')
+plt.title('Distribution of Income Categories')
+plt.show()
+
+#train test split using stratified sampling based on income category
+from sklearn.model_selection import StratifiedShuffleSplit
+split = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(data, data['income_cat']):
+    strat_train_set = data.loc[train_index]
+    strat_test_set = data.loc[test_index]
+print("\n----------- STRATIFIED TRAINING SET INFO -----------")
+print(strat_train_set['income_cat'].value_counts() / len(strat_train_set))
+print("\n----------- STRATIFIED TEST SET INFO -----------")
+print(strat_test_set['income_cat'].value_counts() / len(strat_test_set))
+
+# Remove income_cat attribute so the data is back to its original state
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+# Prepare the data for machine learning algorithms
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+housing_num = housing.drop("ocean_proximity", axis=1)
+housing_cat = housing[["ocean_proximity"]]
+housing_cat_encoded = pd.get_dummies(housing_cat, drop_first=True)
+housing_prepared = pd.concat([housing_num, housing_cat_encoded], axis=1)
